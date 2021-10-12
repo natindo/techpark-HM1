04b86ab35421ecb27c992bd8b3a606f1c70aa203
natindo <lyskov256@gmail.com>
2021-10-12 18:59:15 +0300

    1

---
+++ project/include/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+size_t timer_from(unsigned char from);
+// TODO: Implement `power of` function
+// int custom_pow(int base, int power);
+
+#endif //UTILS_H
+
